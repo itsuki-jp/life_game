ad825e07e3a16a3cf48c55d75b6c60d822e48075
itsuki-jp <itsuki19601@gmail.com>
2021-12-28 23:40:52 +0900

    コメントの追加

--- life_game_tkinter_random.py
+++ life_game_tkinter_random.py
@@ -17,8 +17,9 @@ shape = {
     "octagon": [[0, 0, 0, 1, 1, 0, 0, 0], [0, 0, 1, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 0, 1, 0], [1, 0, 0, 0, 0, 0, 0, 1],
                 [1, 0, 0, 0, 0, 0, 0, 1], [0, 1, 0, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 1, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0]]
 }
-size_w, size_h = 3, 3
+size_w, size_h = 4, 4
 init_pos = [0, 0]  # [y,x]
+max_loop = 1000
 
 # -------------------- Tkinter --------------------
 # ウィンドウの生成
@@ -37,47 +38,46 @@ canvas.pack()
 
 # -------------------- 初期化 --------------------
 # life game
-cells = [[alive if randint(0, 2) else dead for _ in range(w)] for _ in range(h)]
-alive_count = sum([sum(_) for _ in cells])
-
-# tkinter
-# とりあえず、キャンバスの各マスを真っ白にする
+alive_count = 0
+cells = [[dead for _ in range(w)] for _ in range(h)]
 for ty in range(h):
     for tx in range(w):
+        dead_or_alive = randint(0, 2)
         nx = tx * size_w
         ny = ty * size_h
-        canvas.create_rectangle(nx, ny, nx + size_w, ny + size_h, tag=f"{ty}_{tx}",
-                                fill="red" if cells[ty][tx] else "white")
+        if dead_or_alive:  # aliveの場合のみ、四角を生成する->描写量の削減
+            canvas.create_rectangle(nx, ny, nx + size_w, ny + size_h, tag=f"{ty}_{tx}", fill="red")
+            cells[ty][tx] = alive
+            alive_count += 1
 
-# -------------------- 頑張る --------------------
+# -------------------- 更新パート --------------------
 cnt = 0
-while cnt != 1000 and alive_count != 0:
+while cnt != max_loop and alive_count != 0:
     new_cells = copy_cells(cells)
     for i in range(h):  # y
+        temp_i = i * size_h
         for j in range(w):  # x
+            temp_j = j * size_w
+            #  noteより、あるマスに隣接してる生きているマスの数が重要なので、それを数える
             temp = 0
+            for tx, ty in direction:
+                nx, ny = tx + j, ty + i
+                if not 0 <= nx < w or not 0 <= ny < h:
+                    continue
+                if cells[ny][nx] == alive:
+                    temp += 1
+
             if cells[i][j] == dead:
-                for tx, ty in direction:
-                    nx, ny = tx + j, ty + i
-                    if not 0 <= nx < w or not 0 <= ny < h:
-                        continue
-                    if cells[ny][nx] == alive:
-                        temp += 1
-                if temp == 3:
+                if temp == 3:  # 死んでいるセルに隣接する生きたセルが3個→誕生
                     new_cells[i][j] = alive
                     alive_count += 1
-                    canvas.itemconfig(f"{i}_{j}", fill="red")
+                    canvas.create_rectangle(temp_j, temp_i, temp_j + size_w, temp_i + size_h, tag=f"{i}_{j}",
+                                            fill="red")
             else:
-                for tx, ty in direction:
-                    nx, ny = tx + j, ty + i
-                    if not 0 <= nx < w or not 0 <= ny < h:
-                        continue
-                    if cells[ny][nx] == alive:
-                        temp += 1
-                if temp not in (2, 3):
+                if temp not in (2, 3):  # 生きているセルに隣接する生きたセルが2,3個以外→死滅
                     new_cells[i][j] = dead
                     alive_count -= 1
-                    canvas.itemconfig(f"{i}_{j}", fill="white")
+                    canvas.delete(f"{i}_{j}")
     cells = copy_cells(new_cells)
     cnt += 1
     sleep(0.1)
