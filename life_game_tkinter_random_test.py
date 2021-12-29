6f49a48b66a6f87c0d814efb15d367843817e744
itsuki-jp <itsuki19601@gmail.com>
2021-12-29 12:33:53 +0900

    コピー

--- life_game_tkinter_random_test.py
+++ life_game_tkinter_random_test.py
@@ -1,7 +1,6 @@
 import tkinter
 from time import sleep
 from random import randint
-from collections import defaultdict
 
 
 def copy_cells( input_cells ):
@@ -10,16 +9,17 @@ def copy_cells( input_cells ):
 
 
 # -------------------- 変数 --------------------
-h, w = 200, 200
 dead = 0
 alive = 1
+h, w = 150, 150
+size_w, size_h = 4, 4
+init_pos = [0, 0]  # [y,x]
+max_loop = 1000
 direction = [(1, 0), (-1, 0), (0, 1), (0, -1), (-1, -1), (1, -1), (-1, 1), (1, 1)]
 shape = {
     "octagon": [[0, 0, 0, 1, 1, 0, 0, 0], [0, 0, 1, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 0, 1, 0], [1, 0, 0, 0, 0, 0, 0, 1],
                 [1, 0, 0, 0, 0, 0, 0, 1], [0, 1, 0, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 1, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0]]
 }
-size_w, size_h = 4, 4
-init_pos = [0, 0]  # [y,x]
 
 # -------------------- Tkinter --------------------
 # ウィンドウの生成
@@ -28,7 +28,7 @@ root.attributes("-topmost", True)
 root.minsize(width=w * size_w, height=h * size_h)
 
 # Frameウィジェットの生成
-frame = tkinter.Frame(root, width=w * size_w, height=h * size_h, bg="black")
+frame = tkinter.Frame(root, width=w * size_w, height=h * size_h)
 frame.propagate(False)
 frame.pack()
 
@@ -45,41 +45,36 @@ for ty in range(h):
         dead_or_alive = randint(0, 2)
         nx = tx * size_w
         ny = ty * size_h
-        if dead_or_alive:  # alive
-            canvas.create_rectangle(nx, ny, nx + size_w, ny + size_h, tag=f"{ty}_{tx}",
-                                    fill="red")
+        if dead_or_alive:  # aliveの場合のみ、四角を生成する->描写量の削減
+            canvas.create_rectangle(nx, ny, nx + size_w, ny + size_h, tag=f"{ty}_{tx}", fill="red")
             cells[ty][tx] = alive
             alive_count += 1
 
-# -------------------- 頑張る --------------------
+# -------------------- 更新パート --------------------
 cnt = 0
-while cnt != 1000 and alive_count != 0:
+while cnt != max_loop and alive_count != 0:
     new_cells = copy_cells(cells)
     for i in range(h):  # y
         temp_i = i * size_h
         for j in range(w):  # x
             temp_j = j * size_w
+            #  noteより、あるマスに隣接してる生きているマスの数が重要なので、それを数える
             temp = 0
+            for tx, ty in direction:
+                nx, ny = tx + j, ty + i
+                if not 0 <= nx < w or not 0 <= ny < h:
+                    continue
+                if cells[ny][nx] == alive:
+                    temp += 1
+
             if cells[i][j] == dead:
-                for tx, ty in direction:
-                    nx, ny = tx + j, ty + i
-                    if not 0 <= nx < w or not 0 <= ny < h:
-                        continue
-                    if cells[ny][nx] == alive:
-                        temp += 1
-                if temp == 3:
+                if temp == 3:  # 死んでいるセルに隣接する生きたセルが3個→誕生
                     new_cells[i][j] = alive
                     alive_count += 1
                     canvas.create_rectangle(temp_j, temp_i, temp_j + size_w, temp_i + size_h, tag=f"{i}_{j}",
                                             fill="red")
             else:
-                for tx, ty in direction:
-                    nx, ny = tx + j, ty + i
-                    if not 0 <= nx < w or not 0 <= ny < h:
-                        continue
-                    if cells[ny][nx] == alive:
-                        temp += 1
-                if temp not in (2, 3):
+                if temp not in (2, 3):  # 生きているセルに隣接する生きたセルが2,3個以外→死滅
                     new_cells[i][j] = dead
                     alive_count -= 1
                     canvas.delete(f"{i}_{j}")
